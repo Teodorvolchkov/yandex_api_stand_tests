a3e8c8c45517121d13f5c5ca3d59b5ce09378c8c
Teodorvolchkov <fedorvolchkov1998@gmail.com>
2024-12-06 15:04:38 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -10,6 +10,17 @@ def post_new_client_kit (kit_body, auth_token):
                          json=kit_body
                          )
 # В переменную response сохраняется результат
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, json=body, headers=data.headers)  # а здесь заголовки
+
+
+response = post_new_user(data.user_body);
+print(response.status_code)
+
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                             headers=data.headers)
 response = post_new_client_kit(data.kit_body, data.auth_token)
 print(response.status_code)
 
